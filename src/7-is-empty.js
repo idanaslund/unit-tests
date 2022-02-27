6d2c73752b06a8bbc936b1c9d1e682a4a5b24178
idanaslund <idanaslund@users.noreply.github.com>
2022-02-27 18:34:06 +0100

    done with tests

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,11 @@
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
-
+  
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
-
+if (type ==='object' && Object.keys(stringArrayOrObject).length === 0 ) {
+  return true
+}
   return false
 }
